8c130b5bc581fc4695a6baa184764426cf35ac87
林中有神君 <2399526055@qq.com>
2023-06-25 12:26:50 +0800

    新增Comparator排序

---
+++ src/main/java/com/lin/strategy/jdk/Student.java
@@ -0,0 +1,21 @@
+package com.lin.strategy.jdk;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author：铃儿响叮当
+ * @Package：com.lin.strategy.jdk
+ * @Date：2023/6/25 12:09
+ * @Filename：Student
+ * @description:
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+    private String name;
+    private int age;
+    private double score;
+}
